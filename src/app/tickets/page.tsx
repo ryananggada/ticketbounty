4785c1fa152a4ea1a533240b44cc0c087007113c
Ryan Anggada Kho <khoryananggada@gmail.com>
2025-06-23 17:40:01 +0700

    Add features folder

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,21 +1,6 @@
-import { CircleCheckIcon, FileInputIcon, PencilIcon } from 'lucide-react';
-import Link from 'next/link';
 import { Heading } from '@/components/heading';
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
 import { initialTickets } from '@/data';
-import { ticketPath } from '@/paths';
-
-const TICKET_ICONS = {
-  OPEN: <FileInputIcon />,
-  IN_PROGRESS: <PencilIcon />,
-  DONE: <CircleCheckIcon />,
-};
+import { TicketItem } from '@/features/ticket/components/ticket-item';
 
 export default function TicketsPage() {
   return (
@@ -24,26 +9,7 @@ export default function TicketsPage() {
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-from-top">
         {initialTickets.map((ticket) => (
-          <Card key={ticket.id} className="w-full max-w-[420px]">
-            <CardHeader>
-              <CardTitle className="flex gap-x-2">
-                <span>{TICKET_ICONS[ticket.status]}</span>
-                <span className="truncate">{ticket.title}</span>
-              </CardTitle>
-            </CardHeader>
-
-            <CardContent>
-              <span className="line-clamp-3 whitespace-break-spaces">
-                {ticket.content}
-              </span>
-            </CardContent>
-
-            <CardFooter>
-              <Link href={ticketPath(ticket.id)} className="text-sm underline">
-                View
-              </Link>
-            </CardFooter>
-          </Card>
+          <TicketItem key={ticket.id} ticket={ticket} />
         ))}
       </div>
     </div>
---
+++ src/features/constants.tsx
@@ -0,0 +1,7 @@
+import { CircleCheckIcon, FileInputIcon, PencilIcon } from 'lucide-react';
+
+export const TICKET_ICONS = {
+  OPEN: <FileInputIcon />,
+  IN_PROGRESS: <PencilIcon />,
+  DONE: <CircleCheckIcon />,
+};
