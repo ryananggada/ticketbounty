e1118024315aea26e936192be852ffde559181bf
Ryan Anggada Kho <khoryananggada@gmail.com>
2025-06-23 17:18:30 +0700

    Add Heading, Header and Placeholder components

---
+++ src/components/header.tsx
@@ -0,0 +1,30 @@
+import { KanbanIcon } from 'lucide-react';
+import Link from 'next/link';
+import { homePath, ticketsPath } from '@/paths';
+import { buttonVariants } from './ui/button';
+
+const Header = () => {
+  return (
+    <nav className="supports-backdrop-blur:bg-background/60 fixed left-0 right-0 top-0 z-20 border-b bg-background/95 backdrop-blur w-full flex justify-between py-2.5 px-5">
+      <div>
+        <Link
+          href={homePath()}
+          className={buttonVariants({ variant: 'ghost' })}
+        >
+          <KanbanIcon />
+          <h1 className="text-lg font-semibold">TicketBounty</h1>
+        </Link>
+      </div>
+      <div>
+        <Link
+          href={ticketsPath()}
+          className={buttonVariants({ variant: 'default' })}
+        >
+          Tickets
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export { Header };
